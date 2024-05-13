6244a26cc276b34789aa5ffb0b4be08e54de03df
CAGorman <158486231+CAGorman@users.noreply.github.com>
2024-05-13 12:11:03 -0400

    Update Employees_Schema.sql

--- EmployeeSQL/Data Modeling, Schemas, & Analysis/Employees_Schema.sql
+++ EmployeeSQL/Data Modeling, Schemas, & Analysis/Employees_Schema.sql
@@ -1,6 +1,6 @@
 -- Create TITLES table
 CREATE TABLE TITLES (
-    title_id VARCHAR (6)  PRIMARY KEY,
+    title_id VARCHAR (5)  PRIMARY KEY,
     title VARCHAR (15)  NOT NULL
 );
 
@@ -12,12 +12,12 @@ SELECT * from TITLES;
 -- Create EMPLOYEES table
 CREATE TABLE EMPLOYEES (
     emp_no INT   PRIMARY KEY,
-    emp_title_id VARCHAR (6)  NOT NULL,
+    emp_title_id VARCHAR (5)  NOT NULL,
 		FOREIGN KEY(emp_title_id) REFERENCES TITLES (title_id),
     birth_date DATE   NOT NULL,
     first_name VARCHAR (15)  NOT NULL,
     last_name VARCHAR (15)  NOT NULL,
-    sex VARCHAR (3)  NOT NULL,
+    sex VARCHAR (2)  NOT NULL,
     hire_date DATE   NOT NULL
 );
 
@@ -41,8 +41,8 @@ SELECT * from SALARIES;
 
 -- Create DEPARTMENTS table
 CREATE TABLE DEPARTMENTS (
-    dept_no VARCHAR(10)   PRIMARY KEY,
-    dept_name VARCHAR(20)   NOT NULL
+    dept_no VARCHAR(5)   PRIMARY KEY,
+    dept_name VARCHAR(15)   NOT NULL
 );
 
 --View DEPARTMENTS table
@@ -53,7 +53,7 @@ SELECT * from DEPARTMENTS;
 -- Create DEPT_EMP table
 CREATE TABLE DEPT_EMP (
     emp_no INT,
-    dept_no VARCHAR (6),
+    dept_no VARCHAR (5),
 	PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY(emp_no) REFERENCES EMPLOYEES (emp_no),
 	FOREIGN KEY(dept_no) REFERENCES DEPARTMENTS (dept_no)
@@ -66,7 +66,7 @@ SELECT * from DEPT_EMP;
 
 -- Create DEPT_MANAGER table
 CREATE TABLE DEPT_MANAGER (
-    dept_no VARCHAR (6),
+    dept_no VARCHAR (5),
     emp_no INT,
 	PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY(dept_no) REFERENCES DEPARTMENTS (dept_no),
